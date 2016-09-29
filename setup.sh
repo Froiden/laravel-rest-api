97eada0ebf607747a1aef830e5cf218c2be4782d
Shashank Jain <shashank@froiden.com>
2016-09-29 14:58:15 +0530

    Chore: trying to get travis working

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+
 rm -rf laravel
-composer create-project laravel/laravel:5.2
+git clone https://github.com/laravel/laravel
 cd laravel || exit
-composer install --no-interaction
-composer require "$PACKAGE_NAME:dev-master"
+git checkout 5.2
+cp .env.example .env
+php artisan key:generate
+composer update --no-interaction
+composer require $PACKAGE_NAME:dev-master --dev
+
 if [[ -v PACKAGE_PROVIDER ]]; then
-    echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${PACKAGE_PROVIDER})',"; next }1' \
-        config/app.php)" > config/app.php
+    echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${PACKAGE_PROVIDER})',"; next }1' config/app.php)" > config/app.php
 fi
+
 if [[ -v FACADES ]]; then
-    echo "$(awk '/'\''aliases'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${FACADES})',"; next }1' \
-        config/app.php)" > config/app.php
+    echo "$(awk '/'\''aliases'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${FACADES})',"; next }1' config/app.php)" > config/app.php
 fi
+
 cd .. || exit
+
