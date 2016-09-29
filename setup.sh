5f5e68e564d9e7a488d0da1ee7ab1ab4fb0bc379
Shashank Jain <shashank@froiden.com>
2016-09-29 21:34:29 +0530

    Chore: shell script to update composer for autoload

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ git checkout 5.2
 composer install --no-interaction
 cp .env.example .env
 php artisan key:generate
-composer require --dev "$PACKAGE_NAME:dev-master"
+git clone https://github.com/Froiden/laravel-rest-api
 
 if [[ -v PACKAGE_PROVIDER ]]; then
     echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${PACKAGE_PROVIDER})',"; next }1' config/app.php)" > config/app.php
@@ -17,5 +17,12 @@ if [[ -v FACADES ]]; then
     echo "$(awk '/'\''aliases'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${FACADES})',"; next }1' config/app.php)" > config/app.php
 fi
 
-cd .. || exit
+php -r "
+	\$arr = json_decode(file_get_contents(\"composer.json\"), true);
+	\$arr[\"autoload\"][\"psr-4\"][\"Froiden\\\\RestAPI\\\\\"] = \"laravel-rest-api/src\";
+	\$arr[\"autoload\"][\"psr-4\"][\"Froiden\\\\RestAPI\\\\Tests\\\\\"] = \"laravel-rest-api/tests\";
+	file_put_contents(\"composer.json\", json_encode(\$arr));
+"
+composer du
 
+cd .. || exit
