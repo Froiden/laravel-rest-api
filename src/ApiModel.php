d6cd635bbab44b97addcbc578c0f24c4659a8f4f
Shashank Jain <shashank@froiden.com>
2016-12-06 11:46:54 +0530

    Fix: date time issue

--- src/ApiModel.php
+++ src/ApiModel.php
@@ -182,6 +182,9 @@ class ApiModel extends Model
         if (preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2}T(\d{2}):(\d{2}):(\d{2})\\+(\d{2}):(\d{2}))$/', $value)) {
             return Carbon::createFromFormat('Y-m-d\TH:i:s+P', $value);
         }
+        elseif (preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2}T(\d{2}):(\d{2}):(\d{2})\\.(\d{1,3})Z)$/', $value)) {
+            return Carbon::createFromFormat('Y-m-d\TH:i:s.uZ', $value);
+        }
 
         // Finally, we will just assume this date is in the format used by default on
         // the database connection and use that format to create the Carbon object
