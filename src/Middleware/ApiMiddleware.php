ff5ffb5c51515af7c0da7845476f48641b17f0de
Shashank Jain <shashank@froiden.com>
2016-12-14 23:56:41 +0530

    Fix: fix misc issues

--- src/Middleware/ApiMiddleware.php
+++ src/Middleware/ApiMiddleware.php
@@ -3,6 +3,8 @@
 namespace Froiden\RestAPI\Middleware;
 
 use Closure;
+use Froiden\RestAPI\ApiResponse;
+use Froiden\RestAPI\Exceptions\UnauthorizedException;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ApiMiddleware
@@ -13,11 +15,16 @@ class ApiMiddleware
         // Add CORS headers
         $response = $next($request);
 
+        if ($response->getStatusCode() == 403 && $response->getContent() == "Forbidden") {
+            $response = ApiResponse::exception(new UnauthorizedException());
+        }
+
         if (config("api.cors") && !$response instanceof StreamedResponse) {
             $response->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', implode(',', config('api.cors_headers')));
         }
+
 
         return $response;
     }
